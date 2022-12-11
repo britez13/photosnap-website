071f3de92bcf249a2956a0740d78296f65a9423c
Florencio Britez <89874745+britez13@users.noreply.github.com>
2022-12-11 10:32:55 -0300

    Add google font properly

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,10 +27,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <style>
-            @import
-            url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap');
-          </style>
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin>
+          <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet">
         </Head>
         <body>
           <Main />
